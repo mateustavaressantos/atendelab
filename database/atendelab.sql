08c4b730904b2cc689f632742bc4603f467d958a
mateustavaressantos <m76jec@gmail.com>
2026-06-03 22:09:02 -0300

    Criação do CRUD para a tabela Usuarios

--- database/atendelab.sql
+++ database/atendelab.sql
@@ -8,7 +8,7 @@ CREATE TABLE usuarios (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    perfil ENUM('admin', 'atendente') DEFAULT 'atendente',
+    perfil ENUM('admin', 'aluno', 'atendente') DEFAULT 'atendente';
     status ENUM('ativo', 'inativo') DEFAULT 'ativo',
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
